be63fa93d03cc59d51e404c8c768636c46577849
pwilkes17 <pwilkes17@yahoo.com>
2015-11-05 20:22:56 -0500

    styled dropdown bar

--- js/main.js
+++ js/main.js
@@ -4,22 +4,70 @@ $(document).ready(function(){
 			.toggleClass("rotate");
 		$('.menu-shown').toggleClass("menu-on");
 	});
+
 	$('.resume-box').on('mouseenter', function(){
 		$(this).addClass('inset');
 		
 	});
+
 	$('.resume-box').on('mouseleave', function(){
 		$(this).removeClass('inset');
 		
 	});
+
 	$('.about-div').on('mouseenter', function(){
 		$(this).addClass('inset');
 		
 	});
+
 	$('.about-div').on('mouseleave', function(){
 		$(this).removeClass('inset');
 		
 	});
+
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top - 88
+	    }, 900, 'swing');
+	});
+
+	var waypoint = $('#about').waypoint({
+		handler: function(direction) {
+			$('select option:contains(About)').attr('selected', true);
+		},
+		
+			offset: '10%'
+		
+	});
+  
+
+	var waypoint = $('#video').waypoint({
+		handler: function(direction) {
+			$('select option:contains(Video)').attr('selected', true);
+		},
+		
+			offset: '20%'
+		
+	});
+	var waypoint = $('#resume').waypoint({
+		handler: function(direction) {
+			$('select option:contains(Resume)').attr('selected', true);
+		},
+			offset: '20%'
+	});
+
+	var waypoint = $('#contact').waypoint({
+		handler: function(direction) {
+			$('select option:contains(Contact)').attr('selected', true);
+		},
+			offset: '50%'
+	}); 
+
 
 
 	$('.collapse').collapse();
